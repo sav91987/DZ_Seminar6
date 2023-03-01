4c3e9ec34ca20888331c5703b04b5bb303c6b0f8
Sotnikov_Andrey <sav91987@ya.ru>
2023-03-01 20:33:04 +0300

    Added Task42

---
+++ Task42/Program.cs
@@ -0,0 +1,20 @@
+﻿System.Console.WriteLine($"Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int numberSaved = number;
+string str = string.Empty;
+
+while (number != 0)
+{
+    str = str + number % 2;
+    number = number / 2;
+}
+
+GetBinNumber(str);
+void GetBinNumber(string strMeth)
+{
+    System.Console.Write($"Число {numberSaved} в двоичной сиситеме равно ");
+    for (int i = str.Length - 1; i >= 0; i--)
+    {
+        System.Console.Write(strMeth[i]);
+    }
+}
--- Task43/Program.cs
+++ Task43/Program.cs
@@ -10,13 +10,13 @@ double k2 = Convert.ToDouble(Console.ReadLine());
 System.Console.WriteLine($"Введите значение b2 для первой прямой: ");
 double b2 = Convert.ToDouble(Console.ReadLine());
 
-if (k1==k2) System.Console.WriteLine("Прямые параллельны");
+if (k1 == k2) System.Console.WriteLine("Прямые параллельны");
 else
 {
     GetCrossPoint(k1, b1, k2, b2);
 }
 
-void GetCrossPoint(double k1,double b1, double k2, double b2)
+void GetCrossPoint(double k1, double b1, double k2, double b2)
 {
     double x = Math.Round(((b2 - b1) / (k1 - k2)), 1);
     double y = Math.Round((k1 * ((b2 - b1) / (k1 - k2)) + b1), 1);
