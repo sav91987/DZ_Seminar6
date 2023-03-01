6a7fbb1b7756250c8c362c660910f38315fece79
Sotnikov_Andrey <sav91987@ya.ru>
2023-03-01 20:29:03 +0300

    Added Task41

---
+++ Task41/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.WriteLine($"Введите количество чисел (M), проверяемых на положительность: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+GetPositive(M);
+void GetPositive(int M)
+{
+    int count = 0;
+
+    for (int i = 1; i <= M; i++)
+    {
+        System.Console.WriteLine($"Введите {i} число");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number >= 0) count++;
+    }
+    System.Console.WriteLine($"Количество положительных чисел равно {count}");
+}
+
